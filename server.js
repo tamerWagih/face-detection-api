707877565ea12a7e6d4dd582af7ae63c11a32264
Tamer Wagih <mrtamerwagih@gmail.com>
2021-05-01 15:47:44 +0300

    adding heroku database to prepare to deploy

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: 'postgresql-flat-02890',
     user: 'postgres',
-    password: '123',
+    password: '',
     database: 'smart-brain',
   },
 });
